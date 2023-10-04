059c1b640c40ee266fff71a3c2b4d6b93df5c056
Jad Bizri <91910175+JadBizri@users.noreply.github.com>
2023-10-04 18:32:05 -0400

    Completed first scripts

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -51,5 +51,49 @@ greetingForm.addEventListener("submit", function (event) {
     greeting.appendChild(greetingText);
 });
 
+const numberForm = document.getElementById("number-form");
+
+// Add an event listener for the form's submit event
+numberForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const numberString = document.getElementById("number").value;
+    let number = Math.round(Math.abs(parseFloat(numberString)));
+    switch (number) {
+        case 1:
+            alert("henagon");
+            break;
+        case 2:
+            alert("digon");
+            break;
+        case 3:
+            alert("trigon");
+            break;
+        case 4:
+            alert("tetragon");
+            break;
+        case 5:
+            alert("pentagon");
+            break;
+        case 6:
+            alert("hexagon");
+            break;
+        case 7:
+            alert("heptagon");
+            break;
+        case 8:
+            alert("octagon");
+            break;
+        case 9:
+            alert("enneagon");
+            break;
+        case 10:
+            alert("decagon");
+            break;
+        default:
+            alert("You failed.");
+    }
+});
+
 //function call
 getDayTime();
